f8935401444cdf5fb1ecce04b02f2f9cb21cc524
lijiwei <1101407798@qq.com>
2019-04-01 16:10:05 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+import "./style.css"
+import img from "./1.jpg"
+
 function Component () {
     let ele = document.createElement("div")
 
     ele.innerHTML = "hello webpack"
+
+    let icon = new Image()
+    icon.src = img
+
+    document.body.appendChild(icon)
 
     return ele
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
 const path = require("path")
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: {
-        app: "./src/index.js"
-        
+        main: './src/index.js'
     },
     output: {
-        filename: "[name].js",
-        path: path.join(__dirname, "dist")
+        filename: '[name].js',
+        path: __dirname + '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.(jpg|png|svg|gif)$/,
+                use: [
+                    "file-loader"
+                ]
+            }
+        ]
     }
 }
