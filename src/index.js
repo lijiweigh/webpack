9350ce5ddfbcab483091b10300272311b9b1fc2d
lijiwei <1101407798@qq.com>
2020-06-27 22:36:40 +0800

    ...

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import _ from "lodash"
+import _ from 'lodash';
+import printMe from './print.js';
 
-import "./style.css"
+  function component() {
+    var element = document.createElement('div');   
+    var btn = document.createElement('button');
 
-function component() {
-    var element = document.createElement('div');
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add("hello")
-  
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    console.log("bbbbb")
+    element.appendChild(btn);
+
     return element;
   }
-  
+
   document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
 
 // import {Configuration} from "webpack"
 
@@ -6,10 +8,20 @@ const path = require("path")
  * @type {Configuration}
  */
 const config = {
-    entry: "./src/index.js",
+    mode: "production",
+    entry: {
+        app: "./src/index.js",
+        print: "./src/print.js"
+    },
     output: {
-        filename: "bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist")
+    },
+    devtool: "eval",
+    devServer: {
+        contentBase: "./dist",
+        compress: true,
+        publicPath: "/public/"
     },
     module: {
         rules: [
@@ -18,7 +30,14 @@ const config = {
                 use: ["style-loader", "css-loader"]
             }
         ]
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "HtmlWebpackPlugin",
+            // template: "./dist/index.html"
+        })
+    ]
 }
 
 module.exports = config
